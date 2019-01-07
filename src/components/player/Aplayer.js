68428eee9168c16dc3038e41c1ef79094bf14561
franklin <ftabarie42@gmail.com>
2019-01-07 10:47:58 +0100

    feat(player): add toggle on spacebar

--- src/components/player/Aplayer.js
+++ src/components/player/Aplayer.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
 import ReactAplayer from 'react-aplayer'
 
+const forbiddenElements = ['input', 'textarea']
+
 class Aplayer extends Component {
+  ap: null
+
+  componentDidMount = () => {
+    document.addEventListener('keyup', this.handleKeyUp)
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  componentWillUnmount = () => {
+    document.removeEventListener('keyup', this.handleKeyUp)
+    document.removeEventListener('keydown', this.handleKeyDowd)
+  }
+
+  handleKeyUp = e => {
+    const element = e.srcElement.localName
+    if (
+      e.keyCode === 32 &&
+      !forbiddenElements.includes(element) &&
+      this.ap &&
+      this.ap.list.audios.length
+    ) {
+      this.ap.toggle()
+    }
+  }
+
+  handleKeyDown = e => {
+    if (e.keyCode === 32 && e.target == document.body) {
+      e.preventDefault()
+    }
+  }
+
   onInit = ap => {
     this.ap = ap
   }
